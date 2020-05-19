bcf503302a8d4e1fa27de9174b7ef5740ee4b1d8
Chris Ramacciotti <chris.j.rama@gmail.com>
2020-05-19 14:37:08 -0500

    Alternate approach to fetching only unique users

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,19 @@
 import _ from "lodash";
 import jph from "../apis/json-placeholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("about to fetch posts");
+  await dispatch(fetchPosts());
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jph.getPosts();
 
