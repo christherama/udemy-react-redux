5b04f424d094049c5bfb4a5059c180be641e3e1d
Chris Ramacciotti <chris.j.rama@gmail.com>
2020-05-24 14:32:06 -0500

    Conditionally shows stream admin buttons

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,3 +1,4 @@
+import history from "../history";
 import streams from "../apis/streams";
 import {
   SIGN_IN,
@@ -29,9 +30,12 @@ export const deleteStream = (id) => async (dispatch) => {
   dispatch({ type: DELETE_STREAM, payload: id });
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
+
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const signIn = (userId) => {
--- streams/client/src/actions/types.js
+++ streams/client/src/actions/types.js
@@ -5,3 +5,4 @@ export const FETCH_STREAMS = "FETCH_STREAMS";
 export const FETCH_STREAM = "FETCH_STREAM";
 export const UPDATE_STREAM = "UPDATE_STREAM";
 export const DELETE_STREAM = "DELETE_STREAM";
+export const NAVIGATE = "NAVIGATE";
